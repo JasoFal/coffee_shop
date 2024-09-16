2251cad3b61a4ec0734bba5b89ea0342bf6a1165
Jason Falk <jasonfalk9@gmail.com>
2024-09-16 14:44:09 -0700

    Add component for coffee items.

---
+++ src/components/Coffee.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Coffee(props) {
+  return (
+    <React.Fragment>
+      <h3>{props.name}</h3>
+      <h3>{props.origin}</h3>
+      <h3>{props.price}</h3>
+      <h3>{props.roast}</h3>
+      <h3>{props.poundsLeft}</h3>
+    </React.Fragment>
+  );
+}
+
+Coffee.propTypes = {
+  name: PropTypes.string,
+  origin: PropTypes.string,
+  price: PropTypes.number,
+  roast: PropTypes.string,
+  id: PropTypes.string
+};
+
+export default Coffee;
